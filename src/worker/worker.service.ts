05c19a9c96f397092c7e567a9c8d662dcd7e8f98
uglynhubme <platon.luk.98@mail.ru>
2024-07-02 21:09:51 +0300

    connecnt prisma

--- src/worker/worker.service.ts
+++ src/worker/worker.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class WorkerService {}
+export class WorkerService {
+    findAll(): string {
+        return 
+    }
+
+
+}
+
+
