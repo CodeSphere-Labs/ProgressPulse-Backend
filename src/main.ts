2d57e8321159e7a22ac505d3b642740e03e336c4
zeroqs <smthq@bk.ru>
2024-07-27 14:56:50 +0300

    docs: add swagger base

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { PrismaClientExceptionFilter } from 'src/prisma-client-exception/prisma-client-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  swaggerInit(app);
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
@@ -15,3 +18,16 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+
+function swaggerInit(app: INestApplication<any>) {
+  const config = new DocumentBuilder()
+    .setTitle('Progress Pulse')
+    .setDescription('The ProgressPulse API description')
+    .setVersion('1.0')
+    .addTag('yoda')
+    .addTag('jedi')
+    .addTag('padawan')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
